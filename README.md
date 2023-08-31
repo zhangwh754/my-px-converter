347634f8ef30c192dd0dd529775b7891680f0817
张文皓 <zhangwh754@163.com>
2023-08-31 11:27:33 +0800

    🎉 init: 初始化

---
+++ README.md
@@ -0,0 +1,10 @@
+# px-converter README
+
+One vscode extension of fast transforming "px" to "Px" and add annotation to avoiding the conflict of prettier and postcss-pxtorem.
+
+一个vscode扩展，快速转换“px”到“Px”，并添加注释，以避免pretier和postcss-px的冲突。
+
+## Features
+
+> Making the line active and using command "convertPxToPx"
+
---
+++ src/extension.ts
@@ -0,0 +1,38 @@
+import * as vscode from "vscode";
+
+/**
+ * @description: 获取缩进
+ * @param {string} text
+ */
+function getIndentation(text: string) {
+  const match = text.match(/^\s*/);
+  return match ? match[0] : "";
+}
+
+export function activate(context: vscode.ExtensionContext) {
+  let disposable = vscode.commands.registerCommand(
+    "my-px-converter.convertPxToPx",
+    () => {
+      const editor = vscode.window.activeTextEditor;
+      if (!editor) {
+        return;
+      }
+
+      const selection = editor.selection;
+      const line = editor.document.lineAt(selection.start.line);
+
+      const indent = getIndentation(line.text);
+
+      const newText = line.text.replace(/(\d+)px/g, "$1Px");
+
+      editor.edit((editBuilder) => {
+        editBuilder.replace(
+          line.range,
+          `${indent}/* prettier-ignore */\n${newText}`
+        );
+      });
+    }
+  );
+
+  context.subscriptions.push(disposable);
+}
